13c388d9b2626678ea7f0ca24fb3c76a99e53eea
kanchawadeamol <kanchawadeamol18@gmail.com>
2022-03-17 14:52:04 +0530

    first contact app commit 3

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -9,7 +9,7 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   // url = 'http://localhost:3000/contacts/';
-  url = 'http://localhost:3000/contacts/';
+  url = 'https://kanchawadeamol.github.io/angular-contact-app/';
 
   addContact(contactData: any) {
     return this.http.post<any>(this.url, contactData).pipe(
